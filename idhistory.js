121ee77162328ee467912d0ae1ab669b90224858
ZenXen7 <karlchristian.ajero@cit.edu>
2024-10-01 21:51:56 +0800

    finished profile card

--- idhistory.js
+++ idhistory.js
@@ -15,7 +15,36 @@ const digIDdata = [
         gender: 'Bisexual',
         bday: 'December 25, 2001'
       },
-      
+      {
+        name: 'Alexandra dela Cruz',
+        gender: 'Transgender',
+        bday: 'January 5, 1925'
+    },
+    {
+      name: 'Alexandra dela Cruz',
+      gender: 'Transgender',
+      bday: 'January 5, 1925'
+  },
+  {
+    name: 'Alexandra dela Cruz',
+    gender: 'Transgender',
+    bday: 'January 5, 1925'
+},
+{
+  name: 'Alexandra dela Cruz',
+  gender: 'Transgender',
+  bday: 'January 5, 1925'
+},
+{
+  name: 'Alexandra dela Cruz',
+  gender: 'Transgender',
+  bday: 'January 5, 1925'
+},
+{
+  name: 'Alexandra dela Cruz',
+  gender: 'Transgender',
+  bday: 'January 5, 1925'
+},
   ];
   
   
